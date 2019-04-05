55515c019ba2e2d6c758cbd96e55fee8334c9e1d
Ding Tao <i@dingtao.org>
2019-04-05 19:56:58 +0800

    stack: Refine stack to make use unaware of lower structure
    Get rid of count, and other stuffs.
    
    Signed-off-by: Ding Tao <i@dingtao.org>

--- include/tm_stack.h
+++ include/tm_stack.h
@@ -11,16 +11,10 @@
 /**
  * tm_stack_t - Teemo stack data structure
  *
- * @count: How many data in the stack
- * @top: Point to the top element of the stack
- * @botton: Point to the bottom element of the stack
  * @priv: Teemo stack private data
  * */
 typedef struct tm_stack_s {
-	unsigned long	count;
-	void		*top;
-	void		*bottom;
-	void		*priv;
+	void *priv;
 } tm_stack_t;
 
 #ifdef __cplusplus
@@ -31,6 +25,7 @@ extern "C" {
  * tm_stack_init - Initialize a stack
  *
  * @stack: Point to the stack
+ *
  * @return:  0 - success
  *	    -1 - error
  * */
@@ -40,6 +35,7 @@ int tm_stack_init(tm_stack_t *stack);
  * tm_stack_destroy - Destroy a stack
  *
  * @stack: Point to the stack
+ *
  * @return:  0 - success
  *	    -1 - error
  * */
@@ -49,7 +45,9 @@ int tm_stack_destroy(tm_stack_t *stack);
  * tm_stack_push - Push one element into stack
  *
  * @stack: Point to the stack
+ *
  * @data: Pointer of the data
+ *
  * @return:  0 - success
  *	    -1 - error
  * */
@@ -59,24 +57,13 @@ int tm_stack_push(tm_stack_t *stack, void *data);
  * tm_stack_pop - Pop top element out of stack
  *
  * @stack: Point to the stack
+ *
+ * @data: Where to save poped data
+ *
  * @return: address - success
  *	       NULL - error
  * */
-void* tm_stack_pop(tm_stack_t *stack);
-
-/**
- * tm_stack_get_count - Get the total number of the data in stack
- *
- * @stack: Point to the stack
- * @return:  > 0 success
- *	      -1 error
- * */
-static inline long tm_stack_get_count(tm_stack_t *stack)
-{
-	if (NULL == stack)
-		return -1;
-	return stack->count;
-}
+int tm_stack_pop(tm_stack_t *stack, void **data);
 
 #ifdef __cplusplus
 }
--- src/tm_stack.c
+++ src/tm_stack.c
@@ -9,125 +9,228 @@
 
 #include "tm_stack.h"
 
+/**
+ * struct tm_stack_attribute_s - Stack attribute
+ *
+ * This structure have some attributes we need to care about, like muti-thread
+ * access mutex or binary compatible flags.
+ *
+ * TODO: Current not used.
+ *
+ * @reserved: Unused value.
+ * */
+typedef struct tm_stack_attribute_s {
+	void *reserved;
+} tm_stack_attribute_t;
+
+/**
+ * struct tm_stack_item_s - Item structure to save elements
+ *
+ * @item: Item address
+ *
+ * @next: Link to next item structure
+ * */
+typedef struct tm_stack_item_s {
+	void *item;
+	struct tm_stack_item_s *next;
+} tm_stack_item_t;
+
+/**
+ * struct tm_stack_priv_s - Private structure of stack
+ *
+ * @attribute: Attribute of this stack
+ *
+ * @top: Top pointer
+ * */
 typedef struct tm_stack_priv_s {
-	long flag;
+	tm_stack_attribute_t *attribute;
+	tm_stack_item_t *top;
 } tm_stack_priv_t;
 
-typedef struct tm_stack_element_s {
-	void *data;
-	void *next;
-} tm_stack_data_t;
 
-bool tm_stack_internal_is_stack_empty(tm_stack_t *stack)
+
+static int tm_stack_internal_push(tm_stack_priv_t *priv, void *data)
 {
-	return true;
-}
-
-bool tm_stack_internal_stack_integrity_check_ok(tm_stack_t *stack)
-{
-	void *data_p;
-	int data_count;
-
-	/* Stack empty */
-	if (0 == stack->count) {
-		/* Both pointer MUST be NULL */
-		if ((NULL == stack->bottom) && (NULL == stack->top)) {
-			return true;
-		} else {
-			return false;
-		}
-	} else {
-		/* Both pointer MUST NOT be NULL */
-		if ((NULL == stack->bottom) || (NULL == stack->top)) {
-			return false;
-		}
-
-		/* One data */
-		if (1 == stack->count) {
-			/* Point to the same address */
-			if (stack->top == stack->bottom) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		data_p = stack->top;
-		data_count = 0;
-		do {
-			data_count++;
-
-			/* Check count first to avoid invalid memory access */
-
-			/* Count number reached */
-			if (data_count == stack->count) {
-				/* MUST point to the last one */
-				if (data_p != stack->bottom) {
-					return false;
-				} else {
-					return true;
-				}
-			}
-
-			/* Reach the bottom */
-			if (data_p == stack->bottom) {
-				if (data_count != stack->count) {
-					return false;
-				} else {
-					return true;
-				}
-			}
-
-			data_p = ((tm_stack_data_t*)data_p)->next;
-		} while (NULL != data_p);
-	}
-	return true;
-}
-
-int tm_stack_init(tm_stack_t *stack)
-{
-	if (NULL == stack)
+	if (NULL == priv) {
 		return -1;
+	}
 
-	stack->count = 0;
-	stack->top = NULL;
-	stack->bottom = NULL;
-	stack->priv = NULL;
+	tm_stack_item_t *item;
+
+	/* Alloc space for data */
+	item = (tm_stack_item_t*)malloc(sizeof(tm_stack_item_t));
+	if (NULL == item) {
+		return -1;
+	}
+
+	/* Save data */
+	item->item = data;
+
+	/* Push item to stack */
+
+	if (NULL == priv->top) {
+		/* Empty stack */
+
+		item->next = NULL;
+
+		priv->top = item;
+	} else {
+		/* None empty stack */
+
+		item->next = priv->top;
+
+		priv->top = item;
+	}
 
 	return 0;
 }
 
+static int tm_stack_internal_pop(tm_stack_priv_t *priv, void **data)
+{
+	if (NULL == priv) {
+		return -1;
+	}
+
+	/* Stack empty */
+	if (NULL == priv->top) {
+		return -1;
+	}
+
+	tm_stack_item_t *item;
+
+	/* Get item */
+	item = priv->top;
+
+	/* Pop item from stack */
+
+	if (NULL == priv->top->next) {
+		/* Stack have only one item */
+
+		priv->top = NULL;
+	} else {
+		/* Stack have more than one item */
+
+		priv->top = priv->top->next;
+	}
+
+	if (NULL != data) {
+		*data = item->item;
+	}
+	free(item);
+
+	return 0;
+}
+
+static int tm_stack_internal_init(tm_stack_priv_t *priv)
+{
+	if (NULL == priv) {
+		return -1;
+	}
+
+	priv->attribute = (tm_stack_attribute_t*)malloc(sizeof(tm_stack_attribute_t));
+	if (NULL == priv->attribute) {
+		return -1;
+	}
+
+	/* TODO attribute currently not used, mark reserved as NULL */
+	priv->attribute->reserved = NULL;
+
+	priv->top = NULL;
+
+	return 0;
+}
+
+static int tm_stack_internal_destroy(tm_stack_priv_t *priv)
+{
+	int ret;
+
+	if (NULL == priv) {
+		return -1;
+	}
+
+	free(priv->attribute);
+
+	/* Remove all item */
+	do {
+		ret = tm_stack_internal_pop(priv, NULL);
+	} while (ret == 0);
+
+	return 0;
+}
+
+int tm_stack_init(tm_stack_t *stack)
+{
+	int ret;
+	tm_stack_priv_t *priv;
+
+	if (NULL == stack) {
+		return -1;
+	}
+
+	priv = (tm_stack_priv_t*)malloc(sizeof(tm_stack_priv_t));
+	if (NULL == priv) {
+		return -1;
+	}
+
+	/* ret == 0 means initial success, other wise error */
+	ret = tm_stack_internal_init(priv);
+	if (ret) {
+		free(priv);
+		return ret;
+	}
+
+	stack->priv = priv;
+
+	return 0;
+}
+
+
 int tm_stack_destroy(tm_stack_t *stack)
 {
-	if (NULL == stack)
+	int ret;
+
+	if (NULL == stack) {
 		return -1;
-	return -1;
+	}
+
+	if (NULL == stack->priv) {
+		return -1;
+	}
+
+	ret = tm_stack_internal_destroy((tm_stack_priv_t*)stack->priv);
+	if (ret) {
+		return ret;
+	}
+
+	free(stack->priv);
+
+	return 0;
 }
 
 
 int tm_stack_push(tm_stack_t *stack, void *data)
 {
-	if (NULL == stack)
+	if (NULL == stack) {
 		return -1;
+	}
 
-	tm_stack_data_t *p;
-
-	p = malloc(sizeof(tm_stack_data_t));
-	if (NULL == p)
+	if (NULL == stack->priv) {
 		return -1;
+	}
 
-	return 0;
+	return tm_stack_internal_push(stack->priv, data);
 }
 
-void* tm_stack_pop(tm_stack_t *stack)
+int tm_stack_pop(tm_stack_t *stack, void **data)
 {
-	if (NULL == stack)
-		return NULL;
+	if (NULL == stack) {
+		return -1;
+	}
 
-	if ((0 == stack->count) ||
-	    (NULL == stack->bottom) ||
-	    (NULL == stack->top))
-		return NULL;
-	return NULL;
+	if (NULL == stack->priv) {
+		return -1;
+	}
+
+	return tm_stack_internal_pop(stack->priv, data);
 }
 
