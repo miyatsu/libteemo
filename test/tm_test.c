a04b80e95b5fa51c4ea623251a5c9506f85dc677
Ding Tao <i@dingtao.org>
2019-04-10 10:09:03 +0800

    test: Use one test file for both stack and queue
    This commit use Makefile as test build facility, should use autotools.
    
    Signed-off-by: Ding Tao <i@dingtao.org>

--- test/tm_test.c
+++ test/tm_test.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 #include "tm_stack.h"
+#include "tm_queue.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +15,10 @@ int main(int argc, char *argv[])
 	int ret;
 	void *data;
 	tm_stack_t stack;
+	tm_queue_t queue;
+	int err_cnt = 0;
 
+	/* Test stack */
 	ret = tm_stack_init(&stack, 0);
 	if (ret) {
 		printf("init error @%d\n", __LINE__);
@@ -36,7 +40,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	int err_cnt = 0;
+	err_cnt = 0;
 	for (i = 0; i < 100; i++) {
 		ret = tm_stack_pop(&stack, &data);
 		if (ret) {
@@ -55,6 +59,49 @@ int main(int argc, char *argv[])
 		printf("destory error @%d\n", __LINE__);
 		exit(-1);
 	}
+
+	/* Test queue */
+	ret = tm_queue_init(&queue, 0);
+	if (ret) {
+		printf("init error @%d\n", __LINE__);
+		exit(-1);
+	}
+
+	for (i = 0; i < 100; i++) {
+		data = malloc(sizeof(int));
+		if (NULL == data) {
+			printf("malloc error @%d\n", __LINE__);
+			exit(-1);
+		}
+		*((int*)data) = i;
+
+		ret = tm_queue_push(&queue, data);
+		if (ret) {
+			printf("push error @%d\n", __LINE__);
+			exit(-1);
+		}
+	}
+
+	err_cnt = 0;
+	for (i = 0; i < 100; i++) {
+		ret = tm_queue_pop(&queue, &data);
+		if (ret) {
+			printf("pop error @%d\n", __LINE__);
+			exit(-1);
+		}
+		if (*((int*)data) != i) {
+			err_cnt++;
+		}
+		free(data);
+	}
+	printf("ERR_CNT = %d\n", err_cnt);
+
+	ret = tm_queue_destroy(&queue);
+	if (ret) {
+		printf("destory error @%d\n", __LINE__);
+		exit(-1);
+	}
+
 
 	return 0;
 }
